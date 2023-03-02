780ec3194a548d1e93f78f90f4b1aa748398a1d2
dhennigar <dhennigar@protonmail.com>
2023-03-02 12:53:02 -0800

    add check for gui to ginit

--- lua/ginit.lua
+++ lua/ginit.lua
@@ -1,8 +1,10 @@
 -- ginit.lua
 -- GUI settings for Nvim-Qt
 
-if vim.loop.os_uname().sysname == "Windows_NT" then
-    vim.cmd [[GuiFont! Cascadia Code]]
-else
-    vim.cmd [[GuiFont! monospace]]
+if vim.g.GuiLoaded == 1 then
+    if vim.loop.os_uname().sysname == "Windows_NT" then
+        vim.cmd [[GuiFont! Cascadia Code]]
+    else
+        vim.cmd [[GuiFont! monospace]]
+    end
 end
