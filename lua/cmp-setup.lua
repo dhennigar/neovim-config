9922dd2242747c82b908204f1147a990c7e64e7e
dhennigar <dhennigar@protonmail.com>
2023-03-04 20:03:41 -0800

    add cmp and lsp lua configs

---
+++ lua/cmp-setup.lua
@@ -0,0 +1,80 @@
+ -- Set up nvim-cmp.
+local cmp = require'cmp'
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+
+cmp.setup({
+    completion = {
+      -- autocomplete = false
+      keyword_length = 4
+    },
+    formatting = {
+    -- formatting options      
+    },
+    snippet = {
+      expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body)
+      end,
+    },
+    window = {
+      -- completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+      ["<Tab>"] = cmp.mapping(function(fallback)
+        if cmp.visible() then
+          local entry = cmp.get_selected_entry()
+            if not entry then
+              cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+              cmp.confirm()
+            else
+              cmp.confirm()
+          end
+        else
+          fallback()
+        end
+      end, {"i","s","c",}),
+      ['<CR>'] = cmp.mapping.confirm(),
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-q>'] = cmp.mapping.abort(),
+      ['<C-space>'] = cmp.mapping(function()
+          if cmp.visible() then
+              cmp.abort()
+          else
+              cmp.complete()
+          end
+      end, {"i","s","c",})
+
+    }),
+    sources = cmp.config.sources({
+      { name = 'cmp_nvim_r', max_item_count = 5 },
+      { name = 'nvim_lsp', max_item_count = 5 },
+      { name = 'vsnip', max_item_count = 5 },
+    }, {
+      { name = 'buffer' },
+    }),
+      -- Use cmdline & path source for ':'
+    cmp.setup.cmdline(':', {
+      completion = {
+          keyword_length = 4
+      },
+      mapping = cmp.mapping.preset.cmdline(),
+      sources = cmp.config.sources({
+        { name = 'path', max_item_count = 5 }
+      }, {
+        { name = 'cmdline', max_item_count = 5 }
+      })
+    })
+})
+
+cmp.event:on(
+    'confirm_done',
+    cmp_autopairs.on_confirm_done()
+)
+
+require 'cmp_nvim_r'.setup({
+      filetypes = {'r', 'rmd', 'quarto'},
+      doc_width = 58
+})
+
+
---
+++ lua/lsp-setup.lua
@@ -0,0 +1,79 @@
+-- lsp bindings
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Displays hover information about the symbol under the cursor
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+    -- Lists all the references 
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Show diagnostics in a floating window
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+    -- Move to the previous diagnostic
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+
+    -- Move to the next diagnostic
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  end
+})
+
+-- lsp set up
+local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+  underline = false,
+  signs = true,
+  update_in_insert = false,
+})
+
+lspconfig.lua_ls.setup {
+    settings = {
+        Lua = {
+            diagnostics = {
+                globals = {'vim', 'require'},
+            },
+        },
+    },
+}
+lspconfig.r_language_server.setup {}
+lspconfig.pyright.setup {}
+lspconfig.clangd.setup {}
+lspconfig.vimls.setup {}
+
